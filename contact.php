d99e47966b262711bcc97d3e91c3892d0c37cc25
John Stratton <jstrat0407@gmail.com>
2017-06-01 23:07:24 -0400

    added files that are currently unused but will be added later

---
+++ contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>Contact Inspired Heights</title><meta name="description" content="Steeplejack contractor offers church and church steeple repair, restoration, renovation, church painting, church windows, church roofs, tuck pointing" /><link href="design/style.css" rel="stylesheet" type="text/css" /><script type="text/javascript" src="js/fade.js"></script></head><body><div id="wrapper">	<div id="header">		<div class="phone"></div>		<div class="logo"><a href="/" title="Inspired Heights"><img src="images/logo.png" width="270" height="50" border="0" alt="Inspired Heights" /></a></div>	</div>	<div id="nav">		<div class="navigation_menu">			<div class="navigation_tcat"></div>			<ul class="menu">				<li><a href="index.php" class="menu_home" title="Home"></a></li>				<li><a href="about.php" class="menu_abou" title="About Us"></a></li>				<li><a href="church-steeples.php" class="menu_cste" title="Church Steeples"></a></li>				<li><a href="church-roofs.php" class="menu_croo" title="Church Roofs"></a></li>				<li><a href="mason_stone-restoration.php" class="menu_mres" title="Mason/Stone Restoration"></a></li>				<li><a href="church-windows.php" class="menu_cwin" title="Church Windows"></a></li>				<li><a href="church-interiors.php" class="menu_cint" title="Church Interiors"></a></li>				<li><a href="mentorship-program.php" class="menu_mpro" title="Mentorship Program"></a></li>			</ul>		</div>		<div class="display"><script type="text/javascript" language="javascript">new fadeshow(fadeimages, 693, 299, 0, 3000, 0);</script></div>		<div class="clear"></div>	</div>	<div id="titlebar">		<h1>Contact Us</h1>		<div class="contact_button"><a href="contact.php" title="Contact Us"></a></div>		<!-- <div class="portfolio_button"><a href="#" title="Project Portfolio"></a></div> -->		<div class="clear"></div>	</div>	<div id="page">		<div class="page_top"></div>			<div class="page_main">				<div class="page_wrap">					
+		<div id="contact-area">
+			<form method="post" action="contact.php">
+				<label for="Name">Name:</label>
+				<input type="text" name="Name" id="Name" />
+				
+				<label for="City">City:</label>
+				<input type="text" name="City" id="City" />
+	
+				<label for="Email">Email:</label>
+				<input type="text" name="Email" id="Email" />
+				
+				<label for="Message">Message:</label><br />
+				<textarea name="Message" rows="20" cols="20" id="Message"></textarea>
+				
+				<script type="text/javascript" src="http://www.google.com/recaptcha/api/challenge?k=6Lc0TfcSAAAAAKlLJEav2m7eBjQQC7q3b8XWc3Pu"></script>
+
+	<noscript>
+  		<iframe src="http://www.google.com/recaptcha/api/noscript?k=6Lc0TfcSAAAAAKlLJEav2m7eBjQQC7q3b8XWc3Pu" height="300" width="500" frameborder="0"></iframe><br/>
+  		<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+  		<input type="hidden" name="recaptcha_response_field" value="manual_challenge"/>
+	</noscript>
+				<input type="submit" name="submitForm" value="Submit" class="submit-button" />
+			</form>
+	</div>
+					<div class="clear"></div>				</div>			</div>		<div class="page_bot"></div>	</div>	<div id="footer">		<div class="botlinks"></div>		<div class="copyright">			All content copyright <strong>Inspired Heights, Inc.</strong>		</div>		<div class="clear"></div>	</div></div></body></html>
